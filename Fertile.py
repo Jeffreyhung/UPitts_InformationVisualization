c8c7c44e4e14c0ff69eda8d7646abf9fe437ddf5
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2018-11-09 20:55:51 -0500

    2018.11.09 add comment

--- Fertile.py
+++ Fertile.py
@@ -9,9 +9,12 @@ for i in lines:
     temp = (i.rstrip('\n').split(','))
     temp[0] = int(round((float(temp[0])+1)*1.5))
     temp[1] = int(float(temp[1])*18+18)
-    temp[5] = int(int(temp[5])+1)
-    temp[6] = int(float(temp[6])*5)
-    temp[7] = int(int(temp[5]) + 1)
+    temp[2] = int(temp[2])
+    temp[3] = int(temp[3])
+    temp[4] = int(temp[4])
+    temp[5] = abs(int(int(temp[5])+1)-2)
+    temp[6] = abs(int(float(temp[6])*5)-5)
+    temp[7] = int(temp[7])+1
     temp[8] = int(float(temp[8])*18)
     if (temp[9] == 'N'):
         temp[9] = 0
@@ -25,8 +28,8 @@ for i in lines:
     [2] Disease     0 false  1 true
     [3] Trauma      0 false  1 true
     [4] Surgical    0 false  1 true
-    [5] Fever       0 less than 3 month,  1 more than 3 months,  2 never
-    [6] Alcohol     1 n/day, 2 1/day, 3 n/week, 4 1/week, 5 never
+    [5] Fever       0 never, 1 more than 3 months,  2 less than 3 month
+    [6] Alcohol     4 n/day, 3 1/day, 2 n/week, 1 1/week, 0 never
     [7] Smoke       0 never, 1 sometime, 2 daily
     [8] Sit Hour    hours(integer)
     [9] Result      0 normal, 1 altered
