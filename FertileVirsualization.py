d6650c5dffa1a3feb16fe5732ea46b47197c92c9
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2018-11-10 20:31:31 -0500

    Scatter Plot with difference in size

--- FertileVirsualization.py
+++ FertileVirsualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from collections import Counter
 import numpy as np
 import seaborn
 import pandas
@@ -25,34 +26,38 @@ for i in fertile:
 
 
 # Function for getting every n-th item in fertile
-def get_n_item(item):
+def get_n_item(item, array=fertile):
     tmp = []
-    for x in fertile:
+    for x in array:
         tmp.append(x[int(item)])
     return tmp
 
-'''
-def scatterplot(a, b, title="", color = "r"):
-    x_data = get_n_item(a)
-    y_data = get_n_item(b)
-    n_data = get_n_item(9)
+
+def scatterplot(a, b):
+    x_data_n = get_n_item(a, normal_data)
+    x_data_a = get_n_item(a, altered_data)
+    y_data_n = get_n_item(b, normal_data)
+    y_data_a = get_n_item(b, altered_data)
     x_label = nameSet[a]
     y_label = nameSet[b]
-
-    # Create the plot object
+# Create the plot object
     _, ax = plt.subplots()
-
-    # s size, c color, marker type of dot, alpha transparency
-    ax.scatter(x_data, n_data, s = 10, c = 'red', alpha = 0.3, marker='x', label = x_label)
-    ax.scatter(y_data, n_data, s = 10, c = 'green', alpha = 0.3, marker='o', label = y_label)
-
-    # Label the axes and provide a title
+# create a list of the sizes, here multiplied by 10 for scale
+    c = Counter(zip(x_data_n, y_data_n))
+    s = [50 * c[(xx, yy)] for xx, yy in zip(x_data_n, y_data_n)]
+# s size, c color, marker type of dot, alpha transparency
+    ax.scatter(x_data_n, y_data_n, s = s, c = 'red', alpha = 0.75, marker='o', label = "Normal")
+    c = Counter(zip(x_data_a, y_data_a))
+    s = [50 * c[(xx, yy)] for xx, yy in zip(x_data_a, y_data_a)]
+    ax.scatter(x_data_a, y_data_a, s = s, c = 'blue', alpha = 0.75, marker='x', label = "Altered")
+# Label the axes and provide a title
     title = 'Scatterplot of ',x_label,' and ',y_label
     ax.set_title(title)
-    ax.set_ylabel(nameSet[9])
+    ax.set_xlabel(nameSet[a])
+    ax.set_ylabel(nameSet[b])
     ax.legend(loc='upper right')
     plt.show()
 
 
+
 scatterplot(6,5)
-'''
